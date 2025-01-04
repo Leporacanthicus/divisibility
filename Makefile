1a02d7b50944d10c623cde69b36e9a18886914b7
Mats Petersson <mats@planetcatfish.com>
2025-01-04 23:15:38 +0000

    Add prime-opt2 and update prime-opt
    The first optimised version missed out 2 as a prime, so check that.
    
    prime-opt2 is a better version, it only checks against known primes,
    similar to how divisibiltiy works. And aside from special case of 2,
    don't try to see if even numbers are primes.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CXX = clang++
 
-all: divisibility prime prime-opt
+all: divisibility prime prime-opt prime-opt2
 
 divisibility: divisibility.cpp
 	${CXX} -O3 -Wall -Wextra -std=c++0x -o $@ $<
@@ -9,6 +9,9 @@ prime: prime.cpp
 	${CXX} -O3 -Wall -Wextra -o $@ $<
 
 prime-opt: prime-opt.cpp
+	${CXX} -O3 -Wall -Wextra -o $@ $<
+
+prime-opt2: prime-opt2.cpp
 	${CXX} -O3 -Wall -Wextra -o $@ $<
 
 
--- prime-opt2.cpp
+++ prime-opt2.cpp
@@ -2,27 +2,23 @@
 #include <cstdint>
 #include <cassert>
 #include <cmath>
+#include <array>
 
-// Slightly optimised prime number finder. Only check odd numbers above 2.
+// Slightly optimised prime number finder. Only check prime numbers from a list.
 
 
 bool isPrime(int num)
 {
     int s = sqrt(num);
 
-    if (num == 2)
+    static std::array<int, 14> primes{ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43 };
+    for(auto d : primes)
     {
-	return true;
-    }
-    
-    if (!(num % 2))
-    {
-	return false;
-    }
-	
-    for(int i = 3; i <= s; i += 2)
-    {
-	if (!(num % i))
+	if (d > s)
+	{
+	    return true;
+	}
+	if (!(num % d))
 	{
 	    return false;   // Not a prime.
 	}
@@ -37,7 +33,11 @@ int main()
     int n = 0;
     for (int j = 0; j < 100000; j++)
     {
-	for(int i = 2; i <= 2208; i++)
+	if (isPrime(2))
+	{
+	    n++;
+	}
+	for(int i = 3; i <= 2208; i+=2)
 	{
 	    if (isPrime(i))
 	    {
